65a5794f77f92a3f80d9c7fe33adfd6d79d66fec
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-21 16:15:17 -0300

    fix: correção para injeção dependencia

--- src/GerenciamentoDeProdutos.Api/Program.cs
+++ src/GerenciamentoDeProdutos.Api/Program.cs
@@ -11,13 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
 builder.Services.AddDbContext<ProdutoContexto>(options => options.UseNpgsql("name=ConnectionStrings:BancoDeDados",
                                                                  b => b.MigrationsAssembly("GerenciamentoDeProdutos.Infra")));
 
 builder.Services.AddScoped<IProdutoServico, ProdutoServico>();
 builder.Services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
+
+var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
