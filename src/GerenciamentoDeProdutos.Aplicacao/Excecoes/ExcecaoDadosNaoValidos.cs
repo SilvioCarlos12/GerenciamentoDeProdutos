e239f0120454249aae26aa112537ff16fd5a89bf
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-21 15:15:16 -0300

    feat: adicionado classe de exceção para tratamento de erro

---
+++ src/GerenciamentoDeProdutos.Aplicacao/Excecoes/ExcecaoDadosNaoValidos.cs
@@ -0,0 +1,9 @@
+﻿namespace GerenciamentoDeProdutos.Aplicacao.Excecoes
+{
+    public sealed class ExcecaoDadosNaoValidos : Exception
+    {
+        public ExcecaoDadosNaoValidos() : base() { }
+        public ExcecaoDadosNaoValidos(string message) : base(message) { }
+        public ExcecaoDadosNaoValidos(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ src/GerenciamentoDeProdutos.Aplicacao/Excecoes/ExcecaoProdutoNaoEncontrado.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoDeProdutos.Aplicacao.Excecao
+{
+    [Serializable]
+    public sealed class ExcecaoProdutoNaoEncontrado : Exception
+    {
+        public ExcecaoProdutoNaoEncontrado() : base() { }
+        public ExcecaoProdutoNaoEncontrado(string message) : base(message) { }
+        public ExcecaoProdutoNaoEncontrado(string message, Exception inner) : base(message, inner) { }
+    }
+}
