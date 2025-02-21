714ee00e3224a154aa8b6ad5ef17204db7fbbba1
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-21 15:34:39 -0300

    feat: adicionado função de atualização na ententidade produto

--- src/GerenciamentoDeProdutos.Dominio/Produto.cs
+++ src/GerenciamentoDeProdutos.Dominio/Produto.cs
@@ -1,4 +1,6 @@
-﻿namespace GerenciamentoDeProdutos.Dominio
+﻿using System.Xml.Linq;
+
+namespace GerenciamentoDeProdutos.Dominio
 {
     public sealed class Produto
     {
@@ -6,5 +8,12 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
+
+        public void AtualizarProduto(string name, decimal price ,int stockQuantity)
+        {
+            Name = name;
+            Price = price;
+            StockQuantity = stockQuantity;
+        }
     }
 }
