597abf51e634bcfb0efb652c60455bcae895401d
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-21 15:16:03 -0300

    feat: adicionado validacao e mapeamento de dto para entidade

--- src/GerenciamentoDeProdutos.Aplicacao/Dtos/ProdutoDto.cs
+++ src/GerenciamentoDeProdutos.Aplicacao/Dtos/ProdutoDto.cs
@@ -1,4 +1,35 @@
-﻿namespace GerenciamentoDeProdutos.Aplicacao.Dtos
+﻿using GerenciamentoDeProdutos.Aplicacao.Excecoes;
+using GerenciamentoDeProdutos.Dominio;
+
+namespace GerenciamentoDeProdutos.Aplicacao.Dtos
 {
-    public record ProdutoDto(string? Name, decimal? Price, int? StockQuantity);
+    public record ProdutoDto(string? Name, decimal? Price, int? StockQuantity)
+    {
+        public virtual Produto ToEntidade()
+        {
+            return new Produto
+            {
+                Name = Name!,
+                Price = Price!.Value,
+                StockQuantity = StockQuantity!.Value
+            };
+        }
+        public void ValidarDados()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new ExcecaoDadosNaoValidos("O Nome é obrigatório");
+            }
+
+            if (Price is null || Price > 0)
+            {
+                throw new ExcecaoDadosNaoValidos("O Price é obrigatório");
+            }
+
+            if (StockQuantity is null || StockQuantity > -1)
+            {
+                throw new ExcecaoDadosNaoValidos("O StockQuantity é obrigatório");
+            }
+        }
+    };
 }
--- src/GerenciamentoDeProdutos.Aplicacao/Dtos/ProdutoSaidaDto.cs
+++ src/GerenciamentoDeProdutos.Aplicacao/Dtos/ProdutoSaidaDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GerenciamentoDeProdutos.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,17 @@ using System.Threading.Tasks;
 
 namespace GerenciamentoDeProdutos.Aplicacao.Dtos
 {
-    public sealed record ProdutoSaidaDto(Guid Id, string Name, decimal? Price, int? StockQuantity) : ProdutoDto(Name, Price, StockQuantity);
+    public sealed record ProdutoSaidaDto(Guid Id, string Name, decimal? Price, int? StockQuantity) : ProdutoDto(Name, Price, StockQuantity)
+    {
+        public override Produto ToEntidade()
+        {
+            return new Produto
+            {
+                ProdutoId = Id,
+                Name = Name!,
+                Price = Price!.Value,
+                StockQuantity = StockQuantity!.Value
+            };
+        }
+    }
 }
