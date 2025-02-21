4b9cdfbcc82ab6c49801dc2396dcae0b116ba9df
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-21 15:34:18 -0300

    feat: adicionado serviço de produto

---
+++ src/GerenciamentoDeProdutos.Aplicacao/IProdutoServico.cs
@@ -0,0 +1,13 @@
+﻿using GerenciamentoDeProdutos.Aplicacao.Dtos;
+
+namespace GerenciamentoDeProdutos.Aplicacao
+{
+    public interface IProdutoServico
+    {
+        Task AdicionarProduto(ProdutoDto produtoDto, CancellationToken cancellationToken);
+        Task RemoverProduto(Guid produtoId, CancellationToken cancellationToken);
+        Task<ProdutoSaidaDto> ObterProdutoPorId(Guid produtoId, CancellationToken cancellationToken);
+        Task<List<ProdutoSaidaDto>> ObterProdutosPorFiltros(ProdutoDto produtoDto, CancellationToken cancellation);
+        Task AtualizarProduto(Guid produtoId, ProdutoDto produtoDto, CancellationToken cancellationToken);
+    }
+}
---
+++ src/GerenciamentoDeProdutos.Aplicacao/ProdutoServico.cs
@@ -0,0 +1,110 @@
+﻿using GerenciamentoDeProdutos.Aplicacao.Dtos;
+using GerenciamentoDeProdutos.Aplicacao.Excecao;
+using GerenciamentoDeProdutos.Infra;
+
+namespace GerenciamentoDeProdutos.Aplicacao
+{
+    public sealed class ProdutoServico : IProdutoServico
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoServico(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public async Task AdicionarProduto(ProdutoDto produtoDto, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _produtoRepositorio.AdicionarProduto(produtoDto.ToEntidade(), cancellationToken);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task AtualizarProduto(Guid produtoId, ProdutoDto produtoDto, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var produto = await _produtoRepositorio.ObterProdutoPorId(produtoId, cancellationToken);
+
+                if(produto is null)
+                {
+                    throw new ExcecaoProdutoNaoEncontrado($"Produto com {produtoId} não foi encontrado");
+                }
+
+                produto.AtualizarProduto(produtoDto.Name!, produtoDto.Price!.Value, produtoDto.StockQuantity!.Value);
+
+                await _produtoRepositorio.AtualizarProduto(produto, cancellationToken);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<ProdutoSaidaDto> ObterProdutoPorId(Guid produtoId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var produto = await _produtoRepositorio.ObterProdutoPorId(produtoId, cancellationToken);
+
+                if (produto is null)
+                {
+                    throw new ExcecaoProdutoNaoEncontrado($"Produto com {produtoId} não foi encontrado");
+                }
+
+               return ProdutoSaidaDto.ToDto(produto);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<ProdutoSaidaDto>> ObterProdutosPorFiltros(ProdutoDto produtoDto, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var produtos = await _produtoRepositorio.ObterProdutoPorFiltro(x =>
+                 (string.IsNullOrEmpty(produtoDto.Name) || x.Name.Contains(produtoDto.Name)) &&
+                 (produtoDto.Price > 0 || x.Price == produtoDto.Price) &&
+                 (produtoDto.StockQuantity < 0 || x.StockQuantity == produtoDto.StockQuantity), cancellationToken);
+
+                return produtos.ConvertAll(ProdutoSaidaDto.ToDto);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task RemoverProduto(Guid produtoId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var produto = await _produtoRepositorio.ObterProdutoPorId(produtoId, cancellationToken);
+
+                if (produto is null)
+                {
+                    throw new ExcecaoProdutoNaoEncontrado($"Produto com {produtoId} não foi encontrado");
+                }
+
+                await _produtoRepositorio.RemoverProduto(produto, cancellationToken);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
